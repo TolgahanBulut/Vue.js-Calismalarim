fc70a69e23bd1ad647c2a6b6def6ef90b6820d91
Tolgahan <tolga_han_345@hotmail.com>
2022-10-25 01:12:05 +0300

    No commit message

--- Ürün Ekleme Projesi/src/components/NewProduct.vue
+++ Ürün Ekleme Projesi/src/components/NewProduct.vue
@@ -13,19 +13,19 @@
           <div class="card-body">
             <div class="form-group">
               <label>Ürün Adı</label>
-              <input type="text" class="form-control" placeholder="adını giriniz">
+              <input type="text" v-model="product.title" class="form-control" placeholder="adını giriniz">
             </div>
             <div class="row">
               <div class="form-group col-md-6">
                 <label>Ürün Adeti</label>
-                <input type="text" class="form-control" placeholder="adetini giriniz">
+                <input type="text" v-model="product.count" class="form-control" placeholder="adetini giriniz">
               </div>
               <div class="form-group col-md-6">
                 <label>Ürün Fiyatı</label>
-                <input type="text" class="form-control" placeholder="fiyatını giriniz">
+                <input type="text" v-model="product.price" class="form-control" placeholder="fiyatını giriniz">
               </div>
             </div>
-            <button class="btn btn-outline-info btn-block">Ekle!</button>
+            <button @click="addProduct" class="btn btn-outline-info btn-block">Ekle!</button>
           </div>
         </div>
       </div>
@@ -33,6 +33,7 @@
 </template>
 
 <script>
+import { eventBus } from '../main'
 export default {
     data() {
       return {
@@ -40,6 +41,7 @@ export default {
             title : null,
             count : null,
             price : null,
+            totalPrice : null,
             selectedImage: null
         }
       }
@@ -48,6 +50,17 @@ export default {
       onChange(e) {
         const file = e.target.files[0];
         this.product.selectedImage = URL.createObjectURL(file);
+      },
+      addProduct(){
+        this.product.totalPrice = this.product.count * this.product.price;
+        eventBus.$emit('productAdded' , this.product);
+        this.product = {
+            title : null,
+            count : null,
+            price : null,
+            totalPrice : null,
+            selectedImage: null
+        }
       }
     }
 }
